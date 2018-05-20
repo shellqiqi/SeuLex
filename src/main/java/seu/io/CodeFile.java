9ec5bdaf3b5ac5cbed63fb70936ce2293b1d83f7
shell7 <shellqiqi@outlook.com>
2018-05-20 18:44:26 +0800

    Add CodeFile to generate code file

---
+++ src/main/java/seu/io/CodeFile.java
@@ -0,0 +1,35 @@
+package seu.io;
+
+import seu.dfa.DFA;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CodeFile {
+
+    private BufferedWriter writer;
+    private LexFile lexFile;
+    private DFA dfa;
+
+    public CodeFile(LexFile lexFile, DFA dfa) {
+        this.lexFile = lexFile;
+        this.dfa = dfa;
+    }
+
+    public void writeFile(String filePath) throws IOException {
+        writer = new BufferedWriter(new FileWriter(filePath));
+        writeHeaders();
+        //TODO: generate static code and DFA transitionTable.
+        writeUserSeg();
+    }
+
+    private void writeHeaders() throws IOException {
+        writer.write(lexFile.headers.toString());
+    }
+
+    private void writeUserSeg() throws IOException {
+        writer.write(lexFile.userSeg.toString());
+    }
+
+}
