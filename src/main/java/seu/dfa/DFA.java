bba53543cb778087b3a8bca131f20de56d02a89b
shell7 <shellqiqi@outlook.com>
2018-05-28 18:48:39 +0800

    Simplify code of DFA

--- src/main/java/seu/dfa/DFA.java
+++ src/main/java/seu/dfa/DFA.java
@@ -11,41 +11,42 @@ public class DFA {
     /* The transition table of the DFA */
     public Vector<Vector<Integer>> transitionTable = new Vector<>();
     /* State to start */
-    public int start = 0;
+    public final static int start = 0;
     /* State to accept and action */
     public HashMap<Integer, String> acceptAction = new HashMap<>();
     /* The closure table of dfa */
-    public Vector<TreeSet<Integer>> closureTable = new Vector<>();
+    private Vector<TreeSet<Integer>> closureTable = new Vector<>();
+    /* The Integrated NFA */
+    private IntegratedNFA nfa;
 
     public DFA(IntegratedNFA nfa) {
-        closureTable.add(getFirstClosureTableByNFA(nfa));
-        while (closureTable.size() > transitionTable.size()) {
-            fillRowOfTransitionTable(nfa, transitionTable.size());
-        }
+        this.nfa = nfa;
 
-        fillActionTable(nfa);
+        closureTable.add(getClosuresOfStart());
+        while (closureTable.size() > transitionTable.size())
+            fillRowOfTransitionTable(transitionTable.size());
+
+        fillActionTable();
     }
 
     /**
      * Get a closure of states which a NFA state can reach by 'ε's
      *
-     * @param nfa a NFA
      * @return output states
      */
-    private TreeSet<Integer> getFirstClosureTableByNFA(IntegratedNFA nfa) {
+    private TreeSet<Integer> getClosuresOfStart() {
         TreeSet<Integer> startSet = new TreeSet<>();
         startSet.add(NFA.start);
-        return getClosureTableByNFA(nfa, startSet);
+        return getClosures(startSet);
     }
 
     /**
      * Get a closure of states which some NFA states can reach by 'ε's
      *
-     * @param nfa         a NFA
      * @param statesOfNFA some input state
      * @return output states
      */
-    private TreeSet<Integer> getClosureTableByNFA(IntegratedNFA nfa, TreeSet<Integer> statesOfNFA) {
+    private TreeSet<Integer> getClosures(TreeSet<Integer> statesOfNFA) {
         Stack<Integer> unCalcStates = new Stack<>();
         TreeSet<Integer> result = new TreeSet<>(statesOfNFA);
         unCalcStates.addAll(statesOfNFA);
@@ -65,30 +66,27 @@ public class DFA {
     /**
      * Get a closure of states which some NFA states can reach by a character and any 'ε's
      *
-     * @param nfa         a NFA
      * @param statesOfNFA input states
      * @param ch          a character
      * @return output states
      */
-    private TreeSet<Integer> moveByNFA(IntegratedNFA nfa, TreeSet<Integer> statesOfNFA, Character ch) {
+    private TreeSet<Integer> move(TreeSet<Integer> statesOfNFA, Character ch) {
         TreeSet<Integer> out = new TreeSet<>();
-        for (Integer i : statesOfNFA)
-            out.addAll(nfa.transitionTable.elementAt(i).elementAt(ch));
-        return getClosureTableByNFA(nfa, out);
+        for (Integer stateOfNFA : statesOfNFA)
+            out.addAll(nfa.transitionTable.elementAt(stateOfNFA).elementAt(ch));
+        return getClosures(out);
     }
 
     /**
      * Fill a row of the transition table
      *
-     * @param nfa        a NFA
      * @param stateOfDFA a DFA state whose row in transition table needs to be added
-     * @return new DFA states produced
      */
-    private void fillRowOfTransitionTable(IntegratedNFA nfa, int stateOfDFA) {
+    private void fillRowOfTransitionTable(int stateOfDFA) {
         Vector<Integer> rowOfTransitionTable = new Vector<>();
 
         for (char i = 0; i < DFAUtil.COLUMNS; i++) {
-            TreeSet<Integer> dfaState = new TreeSet<>(moveByNFA(nfa, closureTable.get(stateOfDFA), i));//nfa states
+            TreeSet<Integer> dfaState = new TreeSet<>(move(closureTable.get(stateOfDFA), i));
             if (dfaState.isEmpty()) {
                 rowOfTransitionTable.add(null);
                 continue;
@@ -107,15 +105,13 @@ public class DFA {
     /**
      * Fill action table from NFA to DFA by connection of closureTable,
      * when a DFA state refers to more than one NFA accept state, take the one who come first.
-     *
-     * @param nfa a NFA
      */
-    private void fillActionTable(IntegratedNFA nfa) {
+    private void fillActionTable() {
         for (int i = 0; i < closureTable.size(); i++) {
             TreeSet<Integer> dfaStates = new TreeSet<>(closureTable.get(i));
-            for (Integer i1 : dfaStates) {
-                if (nfa.accept.get(i1) != null) {
-                    acceptAction.put(i, nfa.accept.get(i1));
+            for (Integer state : dfaStates) {
+                if (nfa.accept.get(state) != null) {
+                    acceptAction.put(i, nfa.accept.get(state));
                     break;
                 }
             }
