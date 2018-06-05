748ab36c32c46fccbaccde4fad545d62cbe17e1b
XUCHENGZHUO <912342675@qq.com>
2018-06-05 10:32:03 +0800

    Finish part of function minimizeDFA and pass the test

--- src/main/java/seu/dfa/DFAUtil.java
+++ src/main/java/seu/dfa/DFAUtil.java
@@ -1,15 +1,20 @@
 package seu.dfa;
 
+import javafx.util.Pair;
 import seu.nfa.IntegratedNFA;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Vector;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
 
 public class DFAUtil {
 
     /* Total columns of transition table */
     public final static int COLUMNS = 128;
 
-    public static DFA integratedNFAtoDFA(IntegratedNFA nfa){
+    public static DFA integratedNFAtoDFA(IntegratedNFA nfa) {
         return new DFA(nfa);
     }
 
@@ -18,8 +23,70 @@ public class DFAUtil {
      *
      * @param dfa a DFA to be minimized
      */
-    public void minimizeDFA(DFA dfa){
-        //TODO: Finish the function
+    public static void minimizeDFA(DFA dfa) {
+        Set<Integer> acceptSet = dfa.acceptAction.keySet();
+        HashSet<Integer> allSet = new HashSet<>();
+        for (int i = 0; i < dfa.transitionTable.size(); i++) {
+            allSet.add(i);
+        }
+        int divide = 1;
+        class Divider<T> {
+            private Vector<Set<T>> deviderSets = new Vector<>();
+
+            private Divider(Set<T> set, Predicate<T> filter) {
+                deviderSets.add(set);
+                devideAt(0, filter);
+            }
+
+            private int devideAt(int i, Predicate<T> filter) {
+                Pair<Set<T>, Set<T>> pair = devide(deviderSets.get(i), filter);
+                if (pair.getKey().isEmpty() || pair.getValue().isEmpty()) return -1;
+                deviderSets.set(i, pair.getKey());
+                deviderSets.add(pair.getValue());
+                return size() - 1;
+            }
+
+            private int size() {
+                return deviderSets.size();
+            }
+
+            private boolean equal(Vector<T> set1, Vector<T> set2) {
+                if (set1.size() != set2.size()) return false;
+                for (int i = 0; i < set1.size(); i++) {
+                    if(getIndex(set1.get(i)) != getIndex(set2.get(i)))
+                        return false;
+                }
+                return true;
+            }
+
+            private int getIndex(T element) {
+                AtomicInteger index = new AtomicInteger(-1);
+                deviderSets.forEach(set -> {
+                    if (set.contains(element)) index.set(deviderSets.indexOf(set));
+                });
+                return index.get();
+            }
+
+            private Pair<Set<T>, Set<T>> devide(Set<T> set, Predicate<T> filter) {
+                HashSet<T> v1 = new HashSet<>(set);
+                v1.removeIf(filter);
+                HashSet<T> v2 = new HashSet<>(set);
+                v2.removeAll(v1);
+                return new Pair<>(v1, v2);
+            }
+        }
+        Divider<Integer> statesDivider = new Divider<>(allSet, acceptSet::contains);
+
+        while (divide < statesDivider.size()) {
+            divide = statesDivider.size();
+            for (int i = 0; i < statesDivider.size(); i++) {
+                int state = (int) statesDivider.deviderSets.get(i).toArray()[0];
+                while (statesDivider.devideAt(i, s ->
+                        statesDivider.equal(dfa.transitionTable.get(state), dfa.transitionTable.get(s))) > 0) {}
+            }
+        }
+        System.out.println(statesDivider.deviderSets);
+        //TODO: delete dupilicated states in DFA.
     }
 
     public static String transitionTableDebugMessage(Vector<Vector<Integer>> transitionTable) {
@@ -31,8 +98,8 @@ public class DFAUtil {
         for (int i = 0; i < transitionTable.size(); i++) {
             builder.append(i).append(": ");
             for (int ch = 0; ch < COLUMNS; ch++) {
-                if(!verbose)
-                    if(transitionTable.elementAt(i).elementAt(ch) == null)
+                if (!verbose)
+                    if (transitionTable.elementAt(i).elementAt(ch) == null)
                         continue;
                 if (ch >= 32 && ch <= 126) {
                     builder.append('\'').append((char) ch).append('\'');
@@ -40,7 +107,7 @@ public class DFAUtil {
                     builder.append(ch);
                 }
                 builder.append("[");
-                if(transitionTable.elementAt(i).elementAt(ch) != null)
+                if (transitionTable.elementAt(i).elementAt(ch) != null)
                     builder.append(transitionTable.elementAt(i).elementAt(ch));
                 builder.append("] ");
             }
--- src/test/java/seu/dfa/DFATest.java
+++ src/test/java/seu/dfa/DFATest.java
@@ -24,4 +24,32 @@ public class DFATest {
         System.out.println(dfa.debugMessage());
     }
 
+    @Test
+    @Ignore
+    public void minimizeDFATest1() throws Exception {
+        Vector<NFA> nfas = new Vector<>();
+        nfas.add(NFAUtil.regExpToNFA("i*f"));
+        nfas.add(NFAUtil.regExpToNFA("if*"));
+        IntegratedNFA infa = new IntegratedNFA();
+        infa.integrate(nfas.elementAt(0), "printf(\"i*f\\n\");");
+        infa.integrate(nfas.elementAt(1), "printf(\"if*\\n\");");
+        DFA dfa = DFAUtil.integratedNFAtoDFA(infa);
+        System.out.println(dfa.debugMessage());
+
+        DFAUtil.minimizeDFA(dfa);
+    }
+
+    @Test
+    @Ignore
+    public void minimizeDFATest2() throws Exception {
+        Vector<NFA> nfas = new Vector<>();
+        nfas.add(NFAUtil.regExpToNFA("(a|b)*abb"));
+        IntegratedNFA infa = new IntegratedNFA();
+        infa.integrate(nfas.elementAt(0), "accept");
+        DFA dfa = DFAUtil.integratedNFAtoDFA(infa);
+        System.out.println(dfa.debugMessage());
+
+        DFAUtil.minimizeDFA(dfa);
+    }
+
 }
