fe45b08c1b506300115f65cdfbf24c9d5b87c072
shell7 <shellqiqi@outlook.com>
2018-06-05 15:08:46 +0800

    DFA start with a specify state

--- src/main/java/seu/io/CodeFileUtil.kt
+++ src/main/java/seu/io/CodeFileUtil.kt
@@ -3,99 +3,102 @@ package seu.io
 import seu.dfa.DFA
 import seu.dfa.DFAUtil
 
-fun generate(dfa: DFA): String {
-    val head = """
-        #include <iostream>
-        #include <string>
-        #include <unordered_map>
-        using namespace std;
-        string yytext;
-        unsigned int yylength;
-        """.trimIndent()
-    return head + '\n' +
-            transitionTable(dfa) + '\n' +
-            acceptFunctions(dfa) + '\n' +
-            actionMap(dfa) + '\n' +
-            yylex() + '\n'
-}
+class CodeFileUtil(private val dfa: DFA) {
 
-fun acceptFunctions(dfa: DFA): String {
-    val builder = StringBuilder()
-    for (entry in dfa.acceptAction) {
+    fun generate(): String {
+        val head = """
+            #include <iostream>
+            #include <string>
+            #include <unordered_map>
+            using namespace std;
+            string yytext;
+            unsigned int yylength;
+            """.trimIndent()
+        return head + '\n' +
+                transitionTable() + '\n' +
+                acceptFunctions() + '\n' +
+                actionMap() + '\n' +
+                yylex() + '\n'
+    }
+
+    fun acceptFunctions(): String {
+        val builder = StringBuilder()
+        for (entry in dfa.acceptAction) {
+            builder.append("""
+                int accept${entry.key}() {
+                    ${entry.value}
+                    return 0;
+                }
+                """.trimIndent())
+            builder.append('\n')
+        }
         builder.append("""
-            int accept${entry.key}() {
-                ${entry.value}
+            int acceptDefault() {
+                cout << yytext << ":unknown" << endl;
                 return 0;
             }
             """.trimIndent())
-        builder.append('\n')
+        return builder.toString()
     }
-    builder.append("""
-        int acceptDefault() {
-            cout << yytext << ":unknown" << endl;
-            return 0;
+
+    fun actionMap(): String {
+        val builder = StringBuilder("unordered_map<int, int(*)()> actionMap = {")
+        for (entry in dfa.acceptAction) {
+            builder.append("\n\t{${entry.key},accept${entry.key}},")
         }
-    """.trimIndent())
-    return builder.toString()
-}
-
-fun actionMap(dfa: DFA): String {
-    val builder = StringBuilder("unordered_map<int, int(*)()> actionMap = {")
-    for (entry in dfa.acceptAction) {
-        builder.append("\n\t{${entry.key},accept${entry.key}},")
+        builder.deleteCharAt(builder.lastIndex)
+        builder.append("\n};")
+        return builder.toString()
     }
-    builder.deleteCharAt(builder.lastIndex)
-    builder.append("\n};")
-    return builder.toString()
-}
 
-fun yylex(): String { // TODO: logic error here!!!
-    return """
-        int yylex(istream& in) {
-            int state = 0;
-            char nextChar;
-            yytext = "";
-            yylength = 0;
-            int lastAccept = -1;
-            unsigned int lastLength = 0;
+    fun yylex(): String {
+        return """
+            int yylex(istream& in) {
+                int state = ${dfa.start};
+                char nextChar;
+                yytext = "";
+                yylength = 0;
+                int lastAccept = -1;
+                unsigned int lastLength = 0;
 
-            while (true) {
-                nextChar = in.get();
-                if (nextChar == EOF) return -1;
-                state = transitionTable[state][nextChar];
-                yytext.push_back(nextChar);
-                yylength++;
-                if (actionMap.find(state) != actionMap.end()) {
-                    lastAccept = state;
-                    lastLength = yylength;
-                }
-                if (state == -1) {
-                    while (yylength > lastLength + (lastAccept == -1)) {
-                        in.unget();
-                        yytext.pop_back();
-                        yylength--;
+                while (true) {
+                    nextChar = in.get();
+                    if (nextChar == EOF) return -1;
+                    state = transitionTable[state][nextChar];
+                    yytext.push_back(nextChar);
+                    yylength++;
+                    if (actionMap.find(state) != actionMap.end()) {
+                        lastAccept = state;
+                        lastLength = yylength;
                     }
-                    if (lastAccept == -1)
-                        return acceptDefault();
-                    else
-                        return actionMap.at(lastAccept)();
+                    if (state == -1) {
+                        while (yylength > lastLength + (lastAccept == -1)) {
+                            in.unget();
+                            yytext.pop_back();
+                            yylength--;
+                        }
+                        if (lastAccept == -1)
+                            return acceptDefault();
+                        else
+                            return actionMap.at(lastAccept)();
+                    }
                 }
+                return 0;
             }
-            return 0;
-        }
-        """.trimIndent()
-}
-
-fun transitionTable(dfa: DFA): String {
-    val builder = StringBuilder()
-    builder.append("const int transitionTable[${dfa.transitionTable.size}][${DFAUtil.COLUMNS}] = {")
-    for (row in dfa.transitionTable) {
-        builder.append("\n\t")
-        for (state in row) {
-            builder.append(state ?: -1).append(',')
-        }
+            """.trimIndent()
     }
-    builder.deleteCharAt(builder.lastIndex)
-    builder.append("\n};")
-    return builder.toString()
+
+    fun transitionTable(): String {
+        val builder = StringBuilder()
+        builder.append("const int transitionTable[${dfa.transitionTable.size}][${DFAUtil.COLUMNS}] = {")
+        for (row in dfa.transitionTable) {
+            builder.append("\n\t")
+            for (state in row) {
+                builder.append(state ?: -1).append(',')
+            }
+        }
+        builder.deleteCharAt(builder.lastIndex)
+        builder.append("\n};")
+        return builder.toString()
+    }
 }
